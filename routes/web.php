e74ea3dafbd5fe48d70880207c02c6309474feac
Diego Rodrigues <diegorodgs@outlook.com>
2024-10-04 17:10:22 -0300

    Criando sessao de usuarios.

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\CarrinhoController;
+use App\Http\Controllers\LoginController;
 
 
+// Site
 Route::get('/',[ SiteController::class,'index'])->name('site.index');
 Route::get('/produto/{slug}',[ SiteController::class,'details'])->name('site.details');
 Route::get('/categoria/{id}',[ SiteController::class, 'categoria'])->name('site.categoria');
 
+// Carrinho de Compras
 Route::get('/carrinho', [CarrinhoController::class, 'carrinhoLista'])->name('site.carrinho');
 Route::post('/carrinho',[CarrinhoController::class, 'adicionarCarrinho'])->name('site.addCarrinho');
 Route::post('/atualizar',[CarrinhoController::class, 'atualizarCarrinho'])->name('site.atualizarCarrinho');
 Route::post('/remover',[CarrinhoController::class, 'removerCarrinho'])->name('site.removerCarrinho');
 Route::get('/limpar',[CarrinhoController::class, 'limparCarrinho'])->name('site.limparCarrinho');
+
+// Login
+Route::view('/login', 'login.login')->name('login.form');
+Route::post('/auth', [LoginController::class, 'auth'])->name('login.auth');
+Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
+Route::get('/register',[LoginController::class, 'create'])->name('login.create');
+
+// Admin
+
+
 
